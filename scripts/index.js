fbecff22472f02a7ed27a62bc1a290dbff6ad322
tajtanvvir1 <tanvirah1223@gmail.com>
2024-02-19 15:04:10 +0600

    Coupon bottun updated

--- scripts/index.js
+++ scripts/index.js
@@ -5,26 +5,27 @@ let countAdd = 0;
 let countSub = 40;
 
 for (const seat of allSeat) {
+
     seat.addEventListener("click", function (e) {
+        if (countAdd > 3) {
+            alert('One person can purchase only 4 tickets.');
+            seat.removeEventListener("click");
+            seat.classList.add('bg-[#F7F8F8]', 'text-[#03071280]');
+            seat.classList.remove('seat-color');
+        }
         countAdd = countAdd + 1;
         countSub = countSub - 1;
         document.getElementById('seat-count').innerText = countAdd;
         document.getElementById('seat-left').innerText = countSub;
-        seat.classList.add('bg-[#1DD100]', 'text-white');
 
-        
+
+        seat.classList.add('bg-[#1DD100]', 'text-white', 'pointer-events-none', 'btn');
+
+
         const seatNo = e.target.innerText;
         const selectedContainer = document.getElementById('selected-container');
 
-        if(countAdd > 4){
-         alert('Invalid - You can purchase only 4 tickets');
-         seat.removeEventListener("click")
-        }
-
-        // seat.removeEventListener("click", handleClick);
-
-        // ---------------------------------- Seat info update on click 
-
+        // ---------------------------------- Seat info update on click
         const economy = "Economy";
         const price = 550;
 
@@ -36,17 +37,14 @@ for (const seat of allSeat) {
         const p3 = document.createElement("p");
         p3.innerText = price;
 
-        
-
         div.appendChild(p);
         div.appendChild(p2);
         div.appendChild(p3);
 
-        
         selectedContainer.appendChild(div)
         div.classList.add('flex', 'justify-between', 'my-2');
-        
-                // ----------------------------------- Total Price update
+
+        // ----------------------------------- Total Price update
         const totalPrice = document.getElementById('total-price');
         totalPriceCount = price * countAdd;
 
@@ -55,30 +53,37 @@ for (const seat of allSeat) {
 
         // ----------------------------------- Grand Total Price
         const grandTotalPrice = document.getElementById('grand-total');
-        const coupon = document.getElementById('coupon-input');
         grandTotalPriceCount = totalPriceCount;
         grandTotalPrice.innerText = grandTotalPriceCount;
 
         // ----------------------------------- Coupon conditions
         if (totalPriceCount >= 2200) {
             const inputBtn = document.getElementById('input-btn');
+            const couponInputContainer = document.getElementById('coupon-container');
+            couponInputContainer.classList.remove('hidden');
             inputBtn.addEventListener('click', function () {
                 // input value
                 const couponInput = document.getElementById('coupon-input').value;
-                console.log(couponInput);
-                if(couponInput === 'NEW15'){
+                
+                if (couponInput === 'NEW15') {
+                    
                     grandTotalPriceCount = totalPriceCount * .85;
                     grandTotalPrice.innerText = grandTotalPriceCount;
-                    console.log(grandTotalPriceCount);
-                    const couponInputContainer = document.getElementById('coupon-container');
                     couponInputContainer.classList.add('hidden');
                 }
-                else if(couponInput === 'Couple 20'){
+                else if (couponInput === 'Couple 20') {
                     grandTotalPriceCount = totalPriceCount * .80;
                     grandTotalPrice.innerText = grandTotalPriceCount;
-                    console.log(grandTotalPriceCount);
-                    const couponInputContainer = document.getElementById('coupon-container');
                     couponInputContainer.classList.add('hidden');
+                }
+                else {
+                    alert("Invalid Coupon, kindly enter right coupon code for discount.");
+                    const couponInputClear = document.getElementById('coupon-input');
+                    const inputBtn = document.getElementById('input-btn');
+                    inputBtn.classList.add('disabled:opacity-50')
+                    inputBtn.addEventListener('click', function(){
+                        couponInputClear.value = "";
+                    })
                 }
             })
         }
@@ -87,3 +92,8 @@ for (const seat of allSeat) {
 
     })
 }
+
+
+const passengerName = document.getElementById('passenger-name').value;
+const passengerNumber = document.getElementById('passenger-number').value;
+const modalBtn = document.getElementById('modal-btn');
